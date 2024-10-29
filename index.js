ca4bdd634e4ea25efab2ccaaf49f8dfc37f7ae1d
raycodepro <joseph.culhane@student.tamdistrict.org>
2024-10-29 13:53:45 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var linkElement = document.getElementById('https://google.com');
+document.createElement('a')
+i = 0
+if( Math.random() < 0.04 ){
+    i = i + 5
+console.log("jackpot")
+    linkElement.href = https://google.com;
+
+}
+if( Math.random() < 0.20 ){
+	i = i + 2
+    console.log("epic")
+}
+if( Math.random() < 0.80 ){
+	i = i + 1
+    console.log("eh"
+}
