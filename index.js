8c7b0a9788400181260a6b5e68fea1ec865b774f
raycodepro <joseph.culhane@student.tamdistrict.org>
2024-10-31 09:01:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //FIGURE OUT HOW TO ADD 3 LINKS HERE, ADDING ONCLICK EVENT LISTENER BELOW \/
 let buttonClick = document.getElementById("clickmebtn1")
 let buttonLog = document.getElementById("log-rolls")
@@ -11,17 +10,23 @@ function buttonClick() {
 if( Math.random() <= 0.04 ){
    i = i + 5
    console.log("jackpot")
+	 documentEl.innerText("jackpot")
 //linkElement.href = https://google.com;
 
 }
 if( Math.random() <= 0.05 ){
 	i = i + 2
     console.log("epic")
+	 documentEl.innerText = epic
 }
 if( Math.random() >= 0.41 ){
 	i = i + 1
-    console.log("eh")
+    console.log("eh")			
+	documentEl.innertext = ("eh")
 }
+}
+function buttonLog() {
+
 }
 //const innerTextOutput = document.getElementById("innerTextOutput");
 //innerTextOutput.value = source.innerText;
