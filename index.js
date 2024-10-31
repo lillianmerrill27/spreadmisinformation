3cf36a06c0e19abf88fb750b6f06ae68bf2860df
raycodepro <joseph.culhane@student.tamdistrict.org>
2024-10-31 09:13:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ i = 0
 
 function buttonClick() {
 if( Math.random() <= 0.04 ){
-   i = i + 5
-   console.log("jackpot")
-	 documentEl.innerText("jackpot")
+   i = i + 5;
+   console.log("jackpot");
+	 documentEl.innerText("jackpot");
+	buttonClick.innerHtml = "jackpot";
 //linkElement.href = https://google.com;
 
 }
 if( Math.random() <= 0.05 ){
-	i = i + 2
-    console.log("epic")
-	 documentEl.innerText = epic
+	i = i + 2;
+    console.log("epic");
+	 documentEl.innerText = ("epic");
+	buttonClick.innerHtml = 'epic';
 }
 if( Math.random() >= 0.41 ){
 	i = i + 1
     console.log("eh")			
 	documentEl.innertext = ("eh")
+	buttonClick.innerHtml = 'eh';
 }
 }
 function buttonLog() {
