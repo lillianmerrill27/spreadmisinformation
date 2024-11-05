b6e9886765827cfde2b75e08ce30168bd473cfbb
Lillian <lillian.merrill27@student.tamdistrict.org>
2024-11-05 14:49:34 -0800

    linking css

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 //FIGURE OUT HOW TO ADD 3 LINKS HERE (POSSIBLY), ADDING ONCLICK EVENT LISTENER BELOW \/
 let buttonClick = document.getElementById("clickmebtn1")
 let buttonLog = document.getElementById("log-rolls")
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 //var linkElement = document.getElementById('https://google.com');
 //document.createElement('a')
 
-let i = 0
+let count = 0
 
 function buttonClickHandler() {
 	let selection = Math.random()
 	if (selection <= 0.04) {
-		i = i + 5;
+		count = count + 5;
 		console.log("JACKPOT");
 		documentEl.innerText("JACKPOT");
-		buttonClick.innerHtml = "JACKPOT";
+		countEl.textContent = count;
 		//linkElement.href = https://google.com;
 	}
 	else if (selection <= 0.09) {
-		i = i + 2;
+		count = count + 2;
 		console.log("epic");
 		documentEl.innerText = ("epic");
-		buttonClick.innerHtml = 'epic';
+		countEl.textContent = count;
 	}
 	else {
-		i = i + 1
+		count = count + 1
 		console.log("eh");
 		documentEl.innertext = ("eh");
-		buttonClick.innerHtml = 'eh';
+		countEl.textContent = count;
 	}
-	
+
 }
 function buttonLog() {
-
+	let countStr = count + " - ";
+	saveEl.textContent += countStr;
+	countEl.textContent = 0;
+	count = 0;
 }
 var body = document.body;
 var hover_element = document.getElementById("hero_hover_animation");
