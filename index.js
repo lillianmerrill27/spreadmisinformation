f38b8f672a900ffeba0c40723497b4e124bcdb7e
Lillian <lillian.merrill27@student.tamdistrict.org>
2024-11-05 14:43:35 -0800

    Co-authored-by: Nikki-Lamb28 <Nikki-Lamb28@users.noreply.github.com> Co-authored-by: raycodepro <raycodepro@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4,39 +4,40 @@ let buttonLog = document.getElementById("log-rolls")
 //var linkElement = document.getElementById('https://google.com');
 //document.createElement('a')
 
-i = 0
+let i = 0
 
 function buttonClickHandler() {
-if( Math.random() <= 0.04 ){
-   i = i + 5;
-   console.log("JACKPOT");
-	 documentEl.innerText("JACKPOT");
-	buttonClick.innerHtml = "JACKPOT";
-//linkElement.href = https://google.com;
-
-}
-if( Math.random() <= 0.05 ){
-	i = i + 2;
-    console.log("epic");
-	 documentEl.innerText = ("epic");
-	buttonClick.innerHtml = 'epic';
-}
-if( Math.random() >= 0.41 ){
-	i = i + 1
-    console.log("eh");			
-	documentEl.innertext = ("eh");
-	buttonClick.innerHtml = 'eh';
-}
+	let selection = Math.random()
+	if (selection <= 0.04) {
+		i = i + 5;
+		console.log("JACKPOT");
+		documentEl.innerText("JACKPOT");
+		buttonClick.innerHtml = "JACKPOT";
+		//linkElement.href = https://google.com;
+	}
+	else if (selection <= 0.09) {
+		i = i + 2;
+		console.log("epic");
+		documentEl.innerText = ("epic");
+		buttonClick.innerHtml = 'epic';
+	}
+	else {
+		i = i + 1
+		console.log("eh");
+		documentEl.innertext = ("eh");
+		buttonClick.innerHtml = 'eh';
+	}
+	
 }
 function buttonLog() {
 
 }
 var body = document.body;
 var hover_element = document.getElementById("hero_hover_animation");
-hover_element.addEventListener("mouseover", function(){
+hover_element.addEventListener("mouseover", function () {
 	body.style.backgroundColor = '#ffd66b'
 });
-hover_element.addEventListener("mouseout", function(){
+hover_element.addEventListener("mouseout", function () {
 	body.style.backgroundColor = '#fff';
 });
 //const innerTextOutput = document.getElementById("innerTextOutput");
@@ -44,7 +45,7 @@ hover_element.addEventListener("mouseout", function(){
 //const button = document.querySelector("button");
 //count = 0
 //button.addEventListener("click", (event) => {
-	//button.textContent = `Click count: ${event.detail}`;
-	//count += 1;
- // });
+//button.textContent = `Click count: ${event.detail}`;
+//count += 1;
+// });
 
