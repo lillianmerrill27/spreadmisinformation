2a100b69bd599b08e7cab66aeb44af0aef4cf032
Lillian <lillian.merrill27@student.tamdistrict.org>
2024-11-14 15:08:57 -0800

    js fix again (panic)

--- index.js
+++ index.js
@@ -2,83 +2,74 @@ let buttonClick = document.getElementById("clickmebtn1")
 let buttonLog = document.getElementById("log-rolls")
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-
-
-var t = document.getElementById("count-el")
-var documentText = document.createTextNode(" - ")
-t.appendChild(documentText)
 //var linkElement = document.getElementById('https://google.com');
 //document.createElement('a')
 
 
+
+
 let count = 0
 
+
 function buttonClickHandler() {
-	if( Math.random() <= 0.004 ){
-	   count = count + 500;
-	   documentEl.innerText = ("JACKPOT");
-		console.log("JACKPOT");
-		countEl.textContent = count;
-	//linkElement.href = https://google.com;
-	
-	
-	}
-	if( Math.random() <= 0.04 ){
-		count = count + 25;
-		console.log("epic");
-		 documentEl.innerText = ("epic");
-		countEl.textContent = count;
-	}
-	
-	
-	if( Math.random() <= 0.09){
-		count = count + 5;
-	}
-	
-	
-	if( Math.random() <= 0.4){
-		count = count + 3;
-	}
-	
-	
-	if( Math.random() >= 0.41 ){
-		count = count + 1
-		console.log("eh");          
-		documentEl.innertext = ("eh");
-		countEl.textContent = count;
-	}
-	
-	
-	
-	
-	}
-	
-	
-	
+    let selection = Math.random()
+    if(selection <= 0.004) {
+        count = count + 500;
+    }
+   
+    if (selection <= 0.04) {
+        count = count + 25;
+        //console.log("JACKPOT");
+        //documentEl.innerText("JACKPOT");
+       // countEl.textContent = count;
+        //linkElement.href = https://google.com;
+    }
+    if (selection <= 0.09) {
+        count = count + 5;
+        //console.log("epic");
+        //documentEl.innerText = ("epic");
+        //countEl.textContent = count;
+    }
+    else if (selection <= 0.4) {
+        count = count + 3;
+    }
+    else {
+        count = count + 1
+        //console.log("eh");
+        //documentEl.innertext = ("eh");
+        //countEl.textContent = count;
+    }
 
 
-
-function buttonLogClickHandler() {
-let countStr = count + " - ";
-	saveEl.textContent += countStr;
-	countEl.textContent = 0;
-	count = 0;
 }
+function logAction() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    //countEl.textContent = 0;
+    count = 0;
+}
+
+
+
+
 
 
 var body = document.body;
 var hover_element = document.getElementById("hero_hover_animation");
-hover_element.addEventListener("mouseover", function(){
+hover_element.addEventListener("mouseover", function () {
     body.style.backgroundColor = '#ffd66b'
 });
-hover_element.addEventListener("mouseout", function(){
+hover_element.addEventListener("mouseout", function () {
     body.style.backgroundColor = '#fff';
 });
-const innerTextOutput = document.getElementById("innerTextOutput");
-innerTextOutput.value = source.innerText;
-const button = document.querySelector("button");
-count = 0
-button.addEventListener("click", (event) => {
-    //button.textContent = `Click count: ${event.detail}`;
-    //count += 1;
-  });
+//const innerTextOutput = document.getElementById("innerTextOutput");
+//innerTextOutput.value = source.innerText;
+//const button = document.querySelector("button");
+//count = 0
+//button.addEventListener("click", (event) => {
+//button.textContent = `Click count: ${event.detail}`;
+//count += 1;
+// });
+
+
+
