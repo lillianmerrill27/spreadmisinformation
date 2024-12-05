ec1248757817e4060a460774611a09dba55b8c7b
Lillian <lillian.merrill27@student.tamdistrict.org>
2024-12-05 15:06:41 -0800

    start js lillian

--- lillian.js
+++ lillian.js
@@ -1,3 +1,13 @@
 //lillian js code here
 // goal = voting system for the Shaming
 
+let votesEl1 = document.getElementById("votes-el1")
+
+console.log(votesEl1)
+
+let votes = 0
+
+function vote() {
+    votesEl1 = votesEl1 + 1
+    votesEl1.innerText = votes
+}
