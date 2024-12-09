44192df94eb5fae17c1261bf593da13b9f09b724
A <A>
2024-12-08 21:53:04 -0800

    This might work...

--- yourownstories.js
+++ yourownstories.js
@@ -3,6 +3,6 @@ let yourName = prompt("Hi! what's your name?")
 let storyOne = document.getElementById("story1")
 function getRandomInt(){
     let luckyNumber = Math.random()
+    console.log(luckyNumber)
+    storyOne.textContent = luckyNumber    
 }
-console.log(luckyNumber)
-storyOne.textContent = luckyNumber
