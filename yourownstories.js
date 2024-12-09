22a9dc393ab0b10d7849f637eada9a2eccdb5a86
A <A>
2024-12-08 21:39:01 -0800

    Trying to get rndm number

--- yourownstories.js
+++ yourownstories.js
@@ -1,2 +1,8 @@
 
 let yourName = prompt("Hi! what's your name?")
+let storyOne = document.getElementById("story1")
+function getRandomInt(){
+    let luckyNumber = Math.random()
+}
+console.log(luckyNumber)
+let storyOne.textContent = luckyNumber
