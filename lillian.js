ff3b1d05e5025d64fef951e0323b8842bdf0c409
Lillian <lillian.merrill27@student.tamdistrict.org>
2024-12-10 14:20:37 -0800

    mr sf is all knowing

--- lillian.js
+++ lillian.js
@@ -1,13 +1,12 @@
 //lillian js code here
 // goal = voting system for the Shaming
 
-let voteEl = document.getElementById(vote-el) 
 
-console.long(voteEl)
+// Voting clicker system - make number go up
+    let count = {}
 
-let vote = 0
-
-function votes() {
-    count = count + 1
-    voteEl.innerText = count 
+    function votes(voteElString) {
+        let voteEl = document.getElementById(voteElString + '-vote-el') 
+        count[voteElString] = (count[voteElString] ?? 0) + 1
+        voteEl.innerText = count[voteElString]
 }
